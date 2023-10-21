d376b501263c9a3a404720fc71bcbd19786e7749
shelken <shelken.pxk@gmail.com>
2023-10-21 23:11:25 +0800

    add lazygit and config lazygit in nvim

--- home-manager/apps/neovim/default.nix
+++ home-manager/apps/neovim/default.nix
@@ -4,6 +4,11 @@
   astronvim-config,
   ...
 }: {
+  # 使用 lazygit
+  imports = [
+    ../lazygit.nix
+  ];
+
   xdg.configFile = {
     # astronvim's config
     "nvim".source = astronvim;
@@ -11,7 +16,7 @@
     # my cusotom astronvim config, astronvim will load it after base config
     # https://github.com/AstroNvim/AstroNvim/blob/v3.32.0/lua/astronvim/bootstrap.lua#L15-L16
     # "astronvim/lua/user".source = astronvim-config;
-     "astronvim/lua/user".source = ./astro-nvim-config;
+    "astronvim/lua/user".source = ./astro-nvim-config;
   };
 
   programs.neovim = {
