db434e3a197c07992ef702dd9001d309d48a5ff8
shelken <shelken.pxk@gmail.com>
2023-10-21 23:53:45 +0800

    change file exporer config

--- home-manager/apps/neovim/astro-nvim-config/plugins/user.lua
+++ home-manager/apps/neovim/astro-nvim-config/plugins/user.lua
@@ -9,4 +9,18 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+    -- File explorer(Custom configs)
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,   -- visible by default
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      }
+    },
 }
