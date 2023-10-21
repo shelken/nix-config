d553b3cde30c2e9a7bcddd0ef93e015952ff3cdf
shelken <shelken.pxk@gmail.com>
2023-10-21 23:27:18 +0800

    add astronvim plugins

--- home-manager/apps/neovim/astro-nvim-config/plugins/community.lua
+++ home-manager/apps/neovim/astro-nvim-config/plugins/community.lua
@@ -6,5 +6,40 @@ return {
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-
+ 
+    { import = "astrocommunity.utility.noice-nvim" },
+    -- Fully featured & enhanced replacement for copilot.vim
+    -- <Tab> work with both auto completion in cmp and copilot
+    -- { import = "astrocommunity.media.vim-wakatime" },
+    -- { import = "astrocommunity.motion.leap-nvim" },
+    -- { import = "astrocommunity.motion.flit-nvim" },
+    { import = "astrocommunity.scrolling.nvim-scrollbar" },
+    { import = "astrocommunity.editing-support.auto-save-nvim" },
+    { import = "astrocommunity.editing-support.todo-comments-nvim" },
+    -- Language Support
+    ---- Frontend & NodeJS
+    -- { import = "astrocommunity.pack.typescript-all-in-one" },
+    -- { import = "astrocommunity.pack.tailwindcss" },
+    -- { import = "astrocommunity.pack.html-css" },
+    -- { import = "astrocommunity.pack.prisma" },
+    -- { import = "astrocommunity.pack.vue" },
+    ---- Configuration Language
+    { import = "astrocommunity.pack.markdown" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.yaml" },
+    { import = "astrocommunity.pack.toml" },
+    ---- Backend
+    { import = "astrocommunity.pack.lua" },
+    { import = "astrocommunity.pack.go" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.java" },
+    { import = "astrocommunity.pack.nix" },  -- manually add config for nix, comment this one.
+    { import = "astrocommunity.pack.proto" },
+    ---- Operation & Cloud Native
+    { import = "astrocommunity.pack.terraform" },
+    { import = "astrocommunity.pack.bash" },
+    { import = "astrocommunity.pack.cmake" },
+    { import = "astrocommunity.pack.cpp" },
+    { import = "astrocommunity.pack.docker" },
 }
