05574d52b698ba4debbd26c32ed9034023c423b0
shelken <shelken.pxk@gmail.com>
2024-03-22 15:33:36 +0800

    fix yabai

--- modules/darwin/wm/default.nix
+++ modules/darwin/wm/default.nix
@@ -2,12 +2,21 @@
 let
   # 固定版本
   yabai = pkgs.yabai.overrideAttrs (old: rec {
-    src = pkgs.fetchFromGitHub {
-      owner = "koekeishiya";
-      repo = "yabai";
-      rev = "v6.0.15";
-      hash = "sha256-buX6FRIXdM5VmYpA80eESDMPf+xeMfJJj0ulyx2g94M=";
-    };
+    version = 6.0.15
+    src = 
+        if pkgs.stdenv.isAarch64
+        then
+          (pkgs.fetchzip {
+            url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+            hash = "sha256-8+jAdwF7Yganvv1NsbtMIBWv0rh9JmHuwLWmwiFmDu4=";
+          })
+        else
+          (pkgs.fetchFromGitHub {
+            owner = "koekeishiya";
+            repo = "yabai";
+            rev = "v${version}";
+            hash = "sha256-buX6FRIXdM5VmYpA80eESDMPf+xeMfJJj0ulyx2g94M=";
+          });
   });
 in
 {
