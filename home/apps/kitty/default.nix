273c9f4700f3dc70c236d0ae5c3e2eb88ed653d6
shelken <shelken.pxk@gmail.com>
2024-04-28 15:31:44 +0800

    🐛 fix kitty

--- home/apps/kitty/default.nix
+++ home/apps/kitty/default.nix
@@ -4,6 +4,7 @@ in {
   programs.kitty = {
     enable = true;
     theme = theme;
+    shellIntegration.mode = "no-rc no-cursor"; # no-cursor make `cursor_shape` config works
     extraConfig = builtins.readFile ./kitty.conf;
   };
 }
