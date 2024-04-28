dac61ad49a14a4b143665e653137744d4513470f
shelken <shelken.pxk@gmail.com>
2024-04-28 18:46:34 +0800

    🔧 direnv config

--- home/apps/dev/direnv.nix
+++ home/apps/dev/direnv.nix
@@ -3,6 +3,12 @@ _: {
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
+    config = {
+      global = {
+        # Hides the rather large block of text that is usually printed when entering the environment.direnv version > 2.34
+        hide_env_diff = true;
+      };
+    };
   };
   # home.packages = [
   #   # devenv.packages."${pkgs.system}".devenv
