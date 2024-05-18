1995c06681a8d0f07607b0a628b6b0f3a0575a1d
shelken <shelken.pxk@gmail.com>
2024-05-18 17:41:20 +0800

    🔨 add var GOPROXY for nix_daemon

--- utils/script/darwin_set_proxy.py
+++ utils/script/darwin_set_proxy.py
@@ -20,6 +20,7 @@ pl = plistlib.loads(NIX_DAEMON_PLIST.read_bytes())
 # set http proxy
 pl["EnvironmentVariables"]["HTTP_PROXY"] = HTTP_PROXY
 pl["EnvironmentVariables"]["HTTPS_PROXY"] = HTTP_PROXY
+pl["EnvironmentVariables"]["GOPROXY"] = "https://goproxy.io"
 
 os.chmod(NIX_DAEMON_PLIST, 0o644)
 NIX_DAEMON_PLIST.write_bytes(plistlib.dumps(pl))
