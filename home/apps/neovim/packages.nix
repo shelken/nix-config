a895d285490351163f44088427eac82e6a310e62
shelken <shelken.pxk@gmail.com>
2024-06-04 17:56:07 +0800

    🐛 add back clang

--- home/apps/neovim/packages.nix
+++ home/apps/neovim/packages.nix
@@ -5,6 +5,7 @@
     # c/c++ tools with clang-tools, the unwrapped version won't
     # add alias like `cc` and `c++`, so that it won't conflict with gcc
     # llvmPackages.clang-unwrapped
+    clang-tools
     gnumake
     # 查询文件内容使用
     ripgrep
