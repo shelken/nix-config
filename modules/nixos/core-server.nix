1434724e6a217139963d40d4014324ad73e4e354
shelken <shelken.pxk@gmail.com>
2024-06-07 21:24:21 +0800

    🔧 update docker registry-mirrors

--- modules/nixos/core-server.nix
+++ modules/nixos/core-server.nix
@@ -133,10 +133,11 @@
       #  "https-proxy" = "http://192.168.6.1:7890";
       #};
       registry-mirrors = [
-        "https://hub-mirror.c.163.com"
+        # "https://hub-mirror.c.163.com"
         "https://mirror.baidubce.com"
-        "https://docker.nju.edu.cn"
-        "https://docker.mirrors.sjtug.sjtu.edu.cn"
+        "https://docker.m.daocloud.io"
+        # "https://docker.nju.edu.cn"
+        # "https://docker.mirrors.sjtug.sjtu.edu.cn"
       ];
     };
     #extraOptions = "--http-proxy 'http://192.168.6.1:7890' --https-proxy 'http://192.168.6.1:7890'";
