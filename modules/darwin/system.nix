89acc58c290b2c46b465e18eb4b9de6dbaeb2bda
shelken <shelken.pxk@gmail.com>
2024-06-17 16:38:26 +0800

    ⬆️ upgrade lock and adjust fonts config

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -187,13 +187,9 @@
 
   # Fonts
   fonts = {
-    # will be removed after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fontDir.enable = true;
-
     # will change to `fonts.packages` after this PR is merged:
-    #   https://github.com/LnL7/nix-darwin/pull/754
-    fonts = with pkgs; [
+    # https://github.com/LnL7/nix-darwin/pull/754
+    packages = with pkgs; [
       # packages = with pkgs; [
       # icon fonts
       material-design-icons
