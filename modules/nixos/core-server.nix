60ed793e7a6ff39c6a2ccd0ccea6b651e31dc2ed
shelken <shelken.pxk@gmail.com>
2024-06-24 23:48:09 +0800

    ♻️ adjust code struct

--- modules/nixos/core-server.nix
+++ modules/nixos/core-server.nix
@@ -1,108 +1,11 @@
-{
-  lib,
-  pkgs,
-  nixpkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ../base.nix
+    ./base
+    ./docker.nix
   ];
-  ###################################################################################
-  #
-  #  NixOS's core configuration suitable for all my machines
-  #
-  ###################################################################################
 
-  # for nix server, we do not need to keep too much generations
-  boot.loader.systemd-boot.configurationLimit = lib.mkDefault 10;
-  # boot.loader.grub.configurationLimit = 10;
-  # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    dates = lib.mkDefault "weekly";
-    options = lib.mkDefault "--delete-older-than 1w";
-  };
-
-  nix.settings = {
-    # Manual optimise storage: nix-store --optimise
-    # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-    auto-optimise-store = true;
-    #    https://nixos.org/manual/nix/stable/command-ref/conf-file#conf-builders-use-substitutes
-    builders-use-substitutes = true;
-    # enable flakes globally
-    experimental-features = ["nix-command" "flakes"];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = lib.mkDefault true;
-
-  # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
-  nix.registry.nixpkgs.flake = nixpkgs;
-  nix.channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
-
-  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
-  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
-  environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
-  # https://github.com/NixOS/nix/issues/9574
-  nix.settings.nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
-
-  # Set your time zone.
-  time.timeZone = "Asia/Shanghai";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  #i18n.defaultLocale = "zh_CN.UTF-8";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "zh_CN.UTF-8";
-  #   LC_IDENTIFICATION = "zh_CN.UTF-8";
-  #   LC_MEASUREMENT = "zh_CN.UTF-8";
-  #   LC_MONETARY = "zh_CN.UTF-8";
-  #   LC_NAME = "zh_CN.UTF-8";
-  #   LC_NUMERIC = "zh_CN.UTF-8";
-  #   LC_PAPER = "zh_CN.UTF-8";
-  #   LC_TELEPHONE = "zh_CN.UTF-8";
-  #   LC_TIME = "zh_CN.UTF-8";
-  # };
-
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = lib.mkDefault false;
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      X11Forwarding = true;
-      PermitRootLogin = "no"; # disable root login
-      PasswordAuthentication = false; # disable password login
-    };
-    openFirewall = true;
-  };
-  # for power management TODO?
-  services = {
-    power-profiles-daemon = {
-      enable = true;
-    };
-    upower.enable = true;
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    curl
-    # aria2
-    git # used by nix flakes
-    neofetch
-    just
-    dig
-    # git-lfs # used by huggingface models
-    #docker_24 # use latest docker for use net-proxy
-    # wayvnc
-
     # create a fhs environment by command `fhs`, so we can run non-nixos packages in nixos! TODO?
     (
       let
@@ -118,28 +21,4 @@
           })
     )
   ];
-
-  # replace default editor with neovim
-  environment.variables.EDITOR = "nvim";
-
-  virtualisation.docker = {
-    enable = true;
-    # start dockerd on boot.
-    # This is required for containers which are created with the `--restart=always` flag to work.
-    enableOnBoot = true;
-    daemon.settings = {
-      #proxies = {
-      #  "http-proxy" = "http://192.168.6.1:7890";
-      #  "https-proxy" = "http://192.168.6.1:7890";
-      #};
-      registry-mirrors = [
-        # "https://hub-mirror.c.163.com"
-        "https://mirror.baidubce.com"
-        "https://docker.m.daocloud.io"
-        # "https://docker.nju.edu.cn"
-        # "https://docker.mirrors.sjtug.sjtu.edu.cn"
-      ];
-    };
-    #extraOptions = "--http-proxy 'http://192.168.6.1:7890' --https-proxy 'http://192.168.6.1:7890'";
-  };
 }
---
+++ modules/nixos/docker.nix
@@ -0,0 +1,22 @@
+{...}: {
+  virtualisation.docker = {
+    enable = true;
+    # start dockerd on boot.
+    # This is required for containers which are created with the `--restart=always` flag to work.
+    enableOnBoot = true;
+    daemon.settings = {
+      #proxies = {
+      #  "http-proxy" = "http://192.168.6.1:7890";
+      #  "https-proxy" = "http://192.168.6.1:7890";
+      #};
+      registry-mirrors = [
+        # "https://hub-mirror.c.163.com"
+        "https://mirror.baidubce.com"
+        "https://docker.m.daocloud.io"
+        # "https://docker.nju.edu.cn"
+        # "https://docker.mirrors.sjtug.sjtu.edu.cn"
+      ];
+    };
+    #extraOptions = "--http-proxy 'http://192.168.6.1:7890' --https-proxy 'http://192.168.6.1:7890'";
+  };
+}
