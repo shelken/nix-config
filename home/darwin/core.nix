d5e626f39dc9ccd430ef3b0905a3bc8e4b6e8e3d
shelken <shelken.pxk@gmail.com>
2024-09-03 15:18:44 +0800

    ✨ add some tools

--- home/darwin/core.nix
+++ home/darwin/core.nix
@@ -7,10 +7,12 @@
   home.packages = with pkgs; [
     gh
     gh-dash
-    glab
-    nali
-    rclone
-    superfile
-    comma
+    glab # gitlab cli
+    nali # `traceroute 1.1.1.1 | nali` show geo location
+    rclone # web driver
+    superfile # like yazi
+    comma # use `, cowsay hello` == `nix run nixpkgs#cowsay -- hello`
+    bandwhich # 查看本地程序网络连接
+    doggo # dig 替代
   ];
 }
