7aa37aeeecdbbc706195331aed92c97eab97108c
shelken <shelken.pxk@gmail.com>
2024-10-12 23:35:52 +0800

    🔧 rsync args

--- home/apps/neovim/default.nix
+++ home/apps/neovim/default.nix
@@ -13,7 +13,7 @@ in {
   ];
 
   home.activation.installAstroNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${astronvim-config}/ ${config.xdg.configHome}/nvim/
+    ${pkgs.rsync}/bin/rsync -avz --delete --chmod=D2755,F744 ${astronvim-config}/ ${config.xdg.configHome}/nvim/
   '';
 
   programs.neovim = {
