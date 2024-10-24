8d4cbf6a2f4261ce89b4fd0b8fbb49c5d9305ee6
shelken <shelken.pxk@gmail.com>
2024-10-24 21:42:27 +0800

    🔧 zellij config

--- home/apps/zellij/default.nix
+++ home/apps/zellij/default.nix
@@ -1,11 +1,28 @@
 {
   myvars,
   lib,
+  pkgs,
   ...
 }: let
   theme = "catppuccin-${lib.strings.toLower myvars.catppuccin_flavor}";
-  origin_file = builtins.readFile ./config.kdl;
+  # origin_file = builtins.readFile ./config.kdl;
+  keybinds-config = builtins.readFile ./keybinds.kdl;
   inherit (lib) concatStrings;
+  zellij-forgot = pkgs.stdenv.mkDerivation rec {
+    pname = "zellij-forgot";
+    version = "0.4.0";
+
+    src = builtins.fetchurl {
+      url = "https://github.com/karimould/zellij-forgot/releases/download/${version}/zellij_forgot.wasm";
+      sha256 = "sha256:1hzdvyswi6gh4ngxnplay69w1n8wlk17yflfpwfhv6mdn0gcmlsr";
+    };
+    phases = ["installPhase"];
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/zellij_forgot.wasm
+    '';
+  };
 in {
   programs.zellij = {
     enable = true;
@@ -21,8 +38,28 @@ in {
       ''
         // 主题配色
         theme "${theme}"
+
+        session_serialization true
+        simplified_ui false
       ''
-      origin_file
+      ''
+        //键盘绑定
+        keybinds {
+          ${keybinds-config}
+
+          // 参考：https://github.com/nixypanda/dotfiles/blob/f5d4bb5a1efd006f1db3e29965a12dea09f10356/modules/zellij/default.nix#L56C11-L63C12
+          // 插件：命令提示
+          shared_except "locked" {
+            bind "Ctrl y" {
+              LaunchOrFocusPlugin "file:${zellij-forgot}/bin/zellij_forgot.wasm" {
+                "LOAD_ZELLIJ_BINDINGS" "true"
+                floating true
+              }
+            }
+          }
+        }
+      ''
+      # origin_file
     ];
   };
 }
