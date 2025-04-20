bf8cefc1d9e8d35c246eb3f7357747fcbc432640
shelken <shelken.pxk@gmail.com>
2025-04-20 15:46:17 +0800

    🔧 virts sakamoto

--- hosts/sakamoto/default.nix
+++ hosts/sakamoto/default.nix
@@ -23,7 +23,7 @@ in {
     tools.rclone.enable = true;
     # tools.music.enable = true; # 音乐刮削相关
     # tools.image.enable = true; # 图片处理
-    # tools.virts.enable = true; # 虚拟机
+    tools.virts.enable = true; # 虚拟机
     # tools.others.enable = true; # 其他：altserver,备份
     tools.cap.enable = true;
 
