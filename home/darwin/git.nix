88a3766be64ea500d0af9b4dc734b380b995ba30
shelken <shelken.pxk@gmail.com>
2025-07-17 16:22:50 +0800

    git credential config

--- home/darwin/git.nix
+++ home/darwin/git.nix
@@ -10,13 +10,26 @@ in {
     programs = {
       gh = {
         enable = true;
+        gitCredentialHelper.enable = false;
       };
       bash.initExtra = ''
         export GITHUB_TOKEN="$(cat ${config.sops.secrets."github/cli-token".path})"
       '';
-      zsh.initExtra = ''
+      zsh.initContent = ''
         export GITHUB_TOKEN="$(cat ${config.sops.secrets."github/cli-token".path})"
       '';
+      git.extraConfig.credential = {
+        # 加空白是先清除system定义的osxkeychain
+        "https://github.com" = {
+          helper = ["" "!gh auth git-credential"];
+        };
+        "https://gist.github.com" = {
+          helper = ["" "!gh auth git-credential"];
+        };
+        "https://mirrors.tuna.tsinghua.edu.cn" = {
+          helper = ["" "!gh auth git-credential"];
+        };
+      };
     };
   };
 }
