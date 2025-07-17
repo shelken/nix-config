f4e008d26b8b768280e1935604400b57fc9cd5fb
shelken <shelken.pxk@gmail.com>
2025-07-18 07:49:52 +0800

    add my nix-cache

--- modules/base.nix
+++ modules/base.nix
@@ -74,7 +74,8 @@
       # cache mirror located in China
       # status: https://mirror.sjtu.edu.cn/
       # "https://mirror.sjtu.edu.cn/nix-channels/store"
-      # status: https://mirrors.ustc.edu.cn/status/
+      # status: https://mirrors.ustc.e1du.cn/status/
+      "https://nix-cache.ooooo.space"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
 
       "https://nix-community.cachix.org"
