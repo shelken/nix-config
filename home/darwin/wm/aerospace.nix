055fbcab2052fc34b5ee3854c390a2f3cd4cbaa3
shelken <shelken.pxk@gmail.com>
2025-09-15 20:44:48 +0800

    aerospace config

--- home/darwin/wm/aerospace.nix
+++ home/darwin/wm/aerospace.nix
@@ -2,6 +2,7 @@
   lib,
   mylib,
   config,
+  pkgs,
   # options,
   ...
 }: let
@@ -17,6 +18,23 @@ in {
     xdg.configFile = {
       "aerospace/aerospace.toml" = {
         source = ./aerospace/aerospace.toml;
+      };
+      "aerospace/scripts/pip-move.sh" = {
+        # source = ./aerospace/scripts;
+        text = ''
+          #!/usr/bin/env bash
+          set -e
+          # Get current workspace
+          current_workspace=$(aerospace list-workspaces --focused)
+          # Move PiP windows to current workspace (handles both "Picture-in-Picture" and "Picture in Picture")
+          aerospace list-windows --all | grep -E "(\| Picture-in-Picture|\| Picture in Picture|\| 画中画|\| 访达)" | awk '{print $1}' | while read window_id; do
+              if [ -n "$window_id" ]; then
+                  aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace" 2&> /dev/null
+              fi
+          done
+        '';
+        force = true;
+        executable = true;
       };
     };
   };
