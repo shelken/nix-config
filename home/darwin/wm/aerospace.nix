22c0494a49de202f38b2abbb04a7c7e49a20598b
shelken <shelken.pxk@gmail.com>
2025-10-03 22:15:11 +0800

    aerospace: update

--- home/darwin/wm/aerospace.nix
+++ home/darwin/wm/aerospace.nix
@@ -29,7 +29,7 @@ in {
           # Move PiP windows to current workspace (handles both "Picture-in-Picture" and "Picture in Picture")
           aerospace list-windows --all | grep -E "(\| Picture-in-Picture|\| Picture in Picture|\| 画中画|\| 访达)" | awk '{print $1}' | while read window_id; do
               if [ -n "$window_id" ]; then
-                  aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace" 2&> /dev/null
+                  aerospace move-node-to-workspace --window-id "$window_id" "$current_workspace"
               fi
           done
         '';
