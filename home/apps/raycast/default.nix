1a6f147a2648b984e024b6f290fcf37658480505
shelken <shelken.pxk@gmail.com>
2025-10-09 09:40:17 +0800

    feat：给raycast增加启动项

--- home/apps/raycast/default.nix
+++ home/apps/raycast/default.nix
@@ -2,16 +2,22 @@
   config,
   lib,
   secrets,
+  mylib,
   ...
-}: let
+}:
+let
   cfg = config.shelken.secrets;
   rayconfig_path = "${secrets}/raycast/latest.rayconfig";
-in {
+in
+{
   config = lib.mkIf cfg.enable {
     xdg.configFile = {
       "raycast/latest.rayconfig" = {
         source = rayconfig_path;
       };
     };
+    home.activation.RaycastLoginItem = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+      mylib.mkLoginItemString { app_name = "Raycast"; }
+    );
   };
 }
