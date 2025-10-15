6db789c50ec5ea45ae373f33a3fa2ce00cfc9143
shelken <shelken.pxk@gmail.com>
2025-10-15 14:24:02 +0800

    raycast custom-commands force link

--- home/darwin/scripts/raycast.nix
+++ home/darwin/scripts/raycast.nix
@@ -3,6 +3,8 @@ let
   customCommandsPath = "${config.home.homeDirectory}/nix-config/home/darwin/scripts/raycast";
 in
 {
-  xdg.configFile."raycast/custom-commands".source =
-    config.lib.file.mkOutOfStoreSymlink customCommandsPath;
+  xdg.configFile."raycast/custom-commands" = {
+    force = true;
+    source = config.lib.file.mkOutOfStoreSymlink customCommandsPath;
+  };
 }
