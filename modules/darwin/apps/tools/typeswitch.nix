b87d02908f4f1fa38740ece30871631d69375415
shelken <shelken.pxk@gmail.com>
2025-10-27 14:04:23 +0800

    update typeswitch

--- modules/darwin/apps/tools/typeswitch.nix
+++ modules/darwin/apps/tools/typeswitch.nix
@@ -36,20 +36,26 @@ in
         "com.apple.finder" = english;
         "com.apple.Spotlight" = english;
         "com.microsoft.VSCode" = english;
-        "com.raycast.macos" = english;
+        "dev.zed.Zed" = english;
+        "com.apple.dt.Xcode" = english;
+        "com.raycast.macos" = english; # raycast
         "net.kovidgoyal.kitty" = english;
-        "tv.parsec.www" = english;
+        "tv.parsec.www" = english; # parsec
+        "com.apple.ScreenSharing" = english;
         ## 浏览器
         "com.apple.Safari" = english;
         "com.google.Chrome" = english;
         "net.imput.helium" = english;
         #-- 中文
-        "com.apple.Notes" = chinese;
-        "com.tencent.xinWeChat" = chinese;
+        "com.apple.Notes" = chinese; # 备忘录
+        "com.tencent.xinWeChat" = chinese; # 微信
         "md.obsidian" = chinese;
         "ru.keepcoder.Telegram" = chinese;
         "com.kangfenmao.CherryStudio" = chinese;
-        "com.openai.chat" = chinese;
+        "com.openai.chat" = chinese; # chatgpt
+        "com.anthropic.claudefordesktop" = chinese; # claude
+        "com.apple.MobileSMS" = chinese; # 信息
+        "com.apple.systempreferences" = chinese; # 系统偏好设置
       };
     };
 
