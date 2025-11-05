20f04fec844944bc209e0381c7b8ed1005be80fb
shelken <shelken.pxk@gmail.com>
2025-11-05 21:01:08 +0800

    fix

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -58,7 +58,7 @@
       # customize finder
       finder = {
         _FXShowPosixPathInTitle = true; # show full path in finder title
-        AppleShowAllExtensions = true; # show all file extensions
+        # AppleShowAllExtensions = true; # show all file extensions
         FXEnableExtensionChangeWarning = false; # disable warning when changing file extension
         QuitMenuItem = true; # enable quit menu item
         ShowPathbar = true; # show path bar
@@ -99,6 +99,9 @@
         NSAutomaticSpellingCorrectionEnabled = false; # disable auto spelling correction(自动拼写检查)
         NSNavPanelExpandedStateForSaveMode = true; # expand save panel by default(保存文件时的路径选择/文件名输入页)
         NSNavPanelExpandedStateForSaveMode2 = true;
+
+        # finder
+        AppleShowAllExtensions = true;
       };
 
       # customize settings that not supported by nix-darwin directly
