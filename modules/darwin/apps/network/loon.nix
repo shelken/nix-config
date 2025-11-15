39ca09431b9f45cf6f65ca81f4b8a194c9deef61
shelken <shelken.pxk@gmail.com>
2025-11-15 12:21:09 +0800

    feat: add loon

---
+++ modules/darwin/apps/network/loon.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  mylib,
+  config,
+  ...
+}:
+let
+  inherit (lib) mkIf;
+  inherit (mylib) mkBoolOpt;
+  cfg = config.shelken.network.loon;
+in
+{
+  options.shelken.network.loon = {
+    enable = mkBoolOpt false "Whether or not to enable.";
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = {
+      taps = [
+        "shelken/tap"
+      ];
+      casks = [
+        "loon"
+      ];
+      brews = [
+      ];
+    };
+    launchd.user.agents.loon = {
+      command = ''"/Applications/Loon.app/Contents/MacOS/Loon"'';
+      serviceConfig.RunAtLoad = true;
+    };
+  };
+}
