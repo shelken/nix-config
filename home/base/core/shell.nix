53e8bc090181d38d804ff96ecc81dae191cd4d58
shelken <shelken.pxk@gmail.com>
2026-01-11 01:08:32 +0800

    lms: add shell env

--- home/base/core/shell.nix
+++ home/base/core/shell.nix
@@ -31,9 +31,10 @@ let
   rustBin = "${config.home.homeDirectory}/.cargo/bin";
   npmBin = "${config.home.homeDirectory}/.npm-global/bin";
   krewBin = "${config.home.homeDirectory}/.krew/bin";
+  lmstudioBin = "${config.home.homeDirectory}/.lmstudio/bin";
 
   envExtra = ''
-    export PATH="$PATH:${localBin}:${goBin}:${rustBin}:${npmBin}:${krewBin}"
+    export PATH="$PATH:${localBin}:${goBin}:${rustBin}:${npmBin}:${krewBin}:${lmstudioBin}"
   '';
 
   initContent = ''
